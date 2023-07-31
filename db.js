1c94d262f6de08d3f1305af4346db4f59397b29f
jdbm69 <104793276+jdbm69@users.noreply.github.com>
2023-07-31 12:24:58 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool ({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: 'jmdb'
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
 });
 
 module.exports = pool;
